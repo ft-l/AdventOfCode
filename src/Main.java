d0cffda296c1597b47b0c037dffa70a8ea9789a3
skaggsw <wskaggs3262@gmail.com>
2018-12-17 14:46:12 -0600

    Complete Day 9

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import problems.problems2018.Day2;
-import problems.problems2018.Day6;
-import problems.problems2018.Day7;
-import problems.problems2018.Day8;
+import problems.problems2018.*;
 
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Day8 day = new Day8();
-        Day8.part1(day.input);
-        Day8.part2(day.input);
+        Day9 day = new Day9();
+        Day9.part1(day.input);
+        Day9.part2(day.input);
     }
 }
---
+++ src/problems/problems2018/Day9.java
@@ -0,0 +1,159 @@
+package problems.problems2018;
+
+import problems.Day;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day9 extends Day {
+
+    public static final String TEST_INPUT = "17 players; last marble is worth 1104 points";
+
+    public Day9() {
+        super(9, 2018);
+    }
+
+    public static void part1(String input) {
+        String inputPatternString = "(\\d+) players; last marble is worth (\\d+) points";
+        Pattern inputPattern = Pattern.compile(inputPatternString);
+        Matcher m = inputPattern.matcher(input);
+        m.find();
+
+        int players = Integer.parseInt(m.group(1));
+        int lastMarbleValue = Integer.parseInt(m.group(2));
+
+        int[] playerScores = new int[players];
+
+        Marble currentMarble = new Marble(0);
+        currentMarble.leftMarble = currentMarble;
+        currentMarble.rightMarble = currentMarble;
+
+        currentMarble.insert(1);
+        currentMarble.insert(2);
+
+        currentMarble = currentMarble.rightMarble;
+
+        int currentMarbleValue = 2;
+        int currentPlayer = 3;
+
+        while (currentMarbleValue < lastMarbleValue) {
+            currentPlayer = (currentPlayer+1)%players;
+            currentMarbleValue++;
+            if (currentMarbleValue % 23 == 0) {
+                currentMarble = currentMarble.leftMarble.leftMarble.leftMarble.leftMarble.leftMarble.leftMarble;
+                playerScores[currentPlayer] += currentMarbleValue+currentMarble.leftMarble.remove().value;
+            } else {
+                currentMarble = currentMarble.rightMarble.insert(currentMarbleValue);
+            }
+        }
+
+        int greatestScore = 0;
+
+        for (int score: playerScores) {
+            if (score > greatestScore) {
+                greatestScore = score;
+            }
+        }
+        System.out.println(greatestScore);
+    }
+
+    public static void part2(String input) {
+        String inputPatternString = "(\\d+) players; last marble is worth (\\d+) points";
+        Pattern inputPattern = Pattern.compile(inputPatternString);
+        Matcher m = inputPattern.matcher(input);
+        m.find();
+
+        int players = Integer.parseInt(m.group(1));
+        int lastMarbleValue = Integer.parseInt(m.group(2))*100;
+
+        long[] playerScores = new long[players];
+
+        Marble currentMarble = new Marble(0);
+        currentMarble.leftMarble = currentMarble;
+        currentMarble.rightMarble = currentMarble;
+
+        currentMarble.insert(1);
+        currentMarble.insert(2);
+
+        currentMarble = currentMarble.rightMarble;
+
+        int currentMarbleValue = 2;
+        int currentPlayer = 3;
+
+        while (currentMarbleValue < lastMarbleValue) {
+            currentPlayer = (currentPlayer+1)%players;
+            currentMarbleValue++;
+            if (currentMarbleValue % 23 == 0) {
+                currentMarble = currentMarble.leftMarble.leftMarble.leftMarble.leftMarble.leftMarble.leftMarble;
+                playerScores[currentPlayer] += currentMarbleValue+currentMarble.leftMarble.remove().value;
+            } else {
+                currentMarble = currentMarble.rightMarble.insert(currentMarbleValue);
+            }
+        }
+
+        long greatestScore = 0;
+
+        for (long score: playerScores) {
+            if (score > greatestScore) {
+                greatestScore = score;
+            }
+        }
+        System.out.println(greatestScore);
+    }
+
+    public static class Marble {
+
+        int value;
+        Marble leftMarble, rightMarble;
+
+        public Marble(int value) {
+            this.value = value;
+        }
+
+        public Marble(int value, int leftMarbleValue, int rightMarbleValue) {
+            this.value = value;
+            leftMarble = new Marble(leftMarbleValue);
+            rightMarble = new Marble(rightMarbleValue);
+        }
+
+        public Marble(int value, Marble leftMarble, Marble rightMarble) {
+            this.value = value;
+            this.leftMarble = leftMarble;
+            this.rightMarble = rightMarble;
+        }
+
+        public Marble insert(int newMarbleValue) {
+            Marble newMarble = new Marble(newMarbleValue, this, this.rightMarble);
+            this.rightMarble.leftMarble = newMarble;
+            this.rightMarble = newMarble;
+            return newMarble;
+        }
+
+        public Marble remove() {
+            this.rightMarble.leftMarble = this.leftMarble;
+            this.leftMarble.rightMarble = this.rightMarble;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            String result = "value = "+value;
+            result += "\nleftMarble = "+leftMarble.value;
+            result += "\nrightMarble = "+rightMarble.value;
+            return result;
+        }
+
+        public String chain() {
+            String result = "";
+            Marble currentMarble = this;
+            while (currentMarble.rightMarble.value != this.value) {
+                result += " " + currentMarble.value;
+                currentMarble = currentMarble.rightMarble;
+            }
+            result += " " + currentMarble.value;
+            return result;
+        }
+    }
+}
