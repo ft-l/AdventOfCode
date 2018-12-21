e906c889389880fb6379669bbf67debbad306f52
skaggsw <wskaggs3262@gmail.com>
2018-12-21 11:12:03 -0600

    Work on Day 15

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Day14 day = new Day14();
-        //Day14.part1(day.input);
-        Day14.part2(day.input);
+        Day15 day = new Day15();
+        Day15.part1(day.TEST_INPUT);
+        //Day15.part2(day.input);
     }
 }
--- src/problems/problems2018/Day15.java
+++ src/problems/problems2018/Day15.java
@@ -3,6 +3,9 @@ package problems.problems2018;
 import problems.Day;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Day15 extends Day {
 
@@ -10,61 +13,155 @@ public class Day15 extends Day {
         super(15, 2018);
     }
 
-    public static char[][] map;
+    public static Point[][] map;
 
-    public static final String TEST_INPUT = "";
+    public static ArrayList<Unit> combatants = new ArrayList<>();
+
+    public static final String TEST_INPUT = "#######\n" +
+                                            "#E..G.#\n" +
+                                            "#...#.#\n" +
+                                            "#.G.#G#\n" +
+                                            "#######";
 
     public static void part1(String input) {
+        map = new Point[input.split("\n")[0].length()][input.split("\n").length];
 
+        System.out.println(map.length);
+        System.out.println(map[0].length);
+
+        for (int x = 0; x < map.length; x++) {
+            for (int y = 0; y < map[0].length; y++) {
+                switch (input.split("\n")[y].charAt(x)) {
+                    case '#':
+                        map[x][y] = new Point(x,y,true);
+                        break;
+                    case '.':
+                        map[x][y] = new Point(x,y,false);
+                        break;
+                    case 'G':
+                        map[x][y] = new Point(x,y,false);
+                        combatants.add(new Goblin(x,y));
+                        break;
+                    case 'E':
+                        map[x][y] = new Point(x,y,false);
+                        combatants.add(new Elf(x,y));
+                        break;
+                }
+            }
+        }
+
+        printMap();
     }
     public static void part2(String input) {
 
     }
 
-    public static class Point {
-        int x, y;
+    public static void printMap() {
+        char[][] mapToPrint = new char[map.length][map[0].length];
+        for (int x = 0; x < map.length; x++) {
+            for (int y = 0; y < map[0].length; y++) {
+                if (map[x][y].isWall) {
+                    mapToPrint[x][y] = '#';
+                } else {
+                    mapToPrint[x][y] = '.';
+                }
+            }
+        }
+        for (Unit combatant: combatants) {
+            if (combatant instanceof Goblin) {
+                mapToPrint[combatant.position.x][combatant.position.y] = 'G';
+            } else {
+                mapToPrint[combatant.position.x][combatant.position.y] = 'E';
+                for (Point point: combatant.position.getReachablePoints()) {
+                    mapToPrint[point.x][point.y] = '?';
+                }
+            }
+        }
 
-        public Point(int x, int y) {
+        for (int y = 0; y < mapToPrint[0].length; y++) {
+            for (int x = 0; x < mapToPrint.length; x++) {
+                System.out.print(mapToPrint[x][y]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static class Point {
+
+        int x, y;
+        boolean isWall;
+
+        public Point(int x, int y, boolean isWall) {
             this.x = x;
             this.y = y;
+            this.isWall = isWall;
         }
 
         public boolean reachable(Point target) {
+            return getReachablePoints().contains(target);
+        }
 
+        public HashSet<Point> getReachablePoints() {
+            HashSet<Point> reachablePoints = new HashSet<>();
+            reachablePoints.addAll(this.availableAdjacentSpaces());
+            reachablePoints.addAll(getReachablePoints(reachablePoints));
+            return reachablePoints;
+        }
+
+        public HashSet<Point> getReachablePoints(HashSet<Point> knownReachablePoints) {
+            System.out.println(knownReachablePoints);
+            for (Point point: knownReachablePoints) {
+                if (!containsAllPoints(knownReachablePoints, point.availableAdjacentSpaces())) {
+                    knownReachablePoints.addAll(point.getReachablePoints(knownReachablePoints));
+                }
+            }
+            return knownReachablePoints;
         }
 
         public ArrayList<Point> availableAdjacentSpaces() {
             ArrayList<Point> points = new ArrayList<>();
-            if (map[x-1][y] != '#') {
-                points.add(new Point(x-1,y));
+            if (!map[x-1][y].isWall) {
+                points.add(new Point(x-1,y, false));
             }
-            if (map[x+1][y] != '#') {
-                points.add(new Point(x+1,y));
+            if (!map[x+1][y].isWall) {
+                points.add(new Point(x+1,y, false));
             }
-            if (map[x][y-1] != '#') {
-                points.add(new Point(x,y-1));
+            if (!map[x][y-1].isWall) {
+                points.add(new Point(x,y-1, false));
             }
-            if (map[x][y+1] != '#') {
-                points.add(new Point(x,y+1));
+            if (!map[x][y+1].isWall) {
+                points.add(new Point(x,y+1, false));
             }
+
+            for (int i = 0; i < points.size(); i++) {
+                for (Unit combatant: combatants) {
+                    if (points.get(i).x == combatant.position.x && points.get(i).y == combatant.position.y) {
+                        points.remove(i);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
             return points;
         }
 
-        public ArrayList<Point> availableMinusPreviousAdjacentSpaces() {
-            ArrayList<Point> points = new ArrayList<>();
-            if (map[x-1][y] != '#') {
-                points.add(new Point(x-1,y));
+        public boolean containsPoint(Set<Point> points, Point target) {
+            for (Point point: points) {
+                if (point.x == target.x && point.y == target.y) {
+                    return true;
+                }
             }
-            if (map[x+1][y] != '#') {
-                points.add(new Point(x+1,y));
+            return false;
+        }
+
+        public boolean containsAllPoints(Set<Point> points, ArrayList<Point> targetPoints) {
+            for (Point targetPoint: points) {
+                if (!containsPoint(points, targetPoint)) {
+                    return false;
+                }
             }
-            if (map[x][y-1] != '#') {
-                points.add(new Point(x,y-1));
-            }
-            if (map[x][y+1] != '#') {
-                points.add(new Point(x,y+1));
-            }
-            return points;
+            return true;
         }
     }
 
@@ -74,7 +171,7 @@ public class Day15 extends Day {
         int attackPower, hitPoints;
 
         public Unit(int x, int y) {
-            position = new Point(x,y);
+            position = new Point(x,y, false);
             attackPower = 3;
             hitPoints = 200;
         }
@@ -87,14 +184,25 @@ public class Day15 extends Day {
 
         }
 
-        public ArrayList<Point> inRange(ArrayList<Unit> units) {
-            return new ArrayList<>();
-        }
-
         public ArrayList<Point> reachable(ArrayList<Point> pointsInRange) {
             for (Point point: pointsInRange) {
-
+                if (!point.reachable(this.position)) {
+                    pointsInRange.remove(point);
+                }
             }
+            return pointsInRange;
+        }
+
+        public ArrayList<Point> inRange(ArrayList<Unit> units) {
+            ArrayList<Point> points = new ArrayList<>();
+            for (Unit enemy: this.getEnemies(units)) {
+                points.addAll(enemy.position.availableAdjacentSpaces());
+            }
+            return points;
+        }
+
+        public ArrayList<Unit> getEnemies(ArrayList<Unit> units) {
+            return new ArrayList<>();
         }
     }
 
@@ -104,15 +212,16 @@ public class Day15 extends Day {
             super(x,y);
         }
 
+
         @Override
-        public ArrayList<Point> inRange(ArrayList<Unit> units) {
-            ArrayList<Point> points = new ArrayList<>();
-            for (Unit elf: units) {
+        public ArrayList<Unit> getEnemies(ArrayList<Unit> units) {
+            ArrayList<Unit> enemies = new ArrayList<>();
+            for (Unit elf : units) {
                 if (elf instanceof Elf) {
-                    points.addAll(elf.position.availableAdjacentSpaces());
+                    enemies.add(elf);
                 }
             }
-            return points;
+            return enemies;
         }
     }
 
@@ -123,25 +232,14 @@ public class Day15 extends Day {
         }
 
         @Override
-        public ArrayList<Point> inRange(ArrayList<Unit> units) {
-            ArrayList<Point> points = new ArrayList<>();
-            for (Unit goblin: units) {
+        public ArrayList<Unit> getEnemies(ArrayList<Unit> units) {
+            ArrayList<Unit> enemies = new ArrayList<>();
+            for (Unit goblin : units) {
                 if (goblin instanceof Goblin) {
-                    if (map[goblin.position.x-1][goblin.position.y] != '#') {
-                        points.add(new Point(goblin.position.x-1,goblin.position.y));
-                    }
-                    if (map[goblin.position.x+1][goblin.position.y] != '#') {
-                        points.add(new Point(goblin.position.x+1,goblin.position.y));
-                    }
-                    if (map[goblin.position.x][goblin.position.y-1] != '#') {
-                        points.add(new Point(goblin.position.x,goblin.position.y-1));
-                    }
-                    if (map[goblin.position.x][goblin.position.y+1] != '#') {
-                        points.add(new Point(goblin.position.x,goblin.position.y+1));
-                    }
+                    enemies.add(goblin);
                 }
             }
-            return points;
+            return enemies;
         }
     }
 }
